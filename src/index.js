1c61f15f2eab5bf3a925bf5808790037c85fdbfe
stroich <stroichmwork@gmail.com>
2023-01-25 18:49:12 +0400

    feat: add js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    const  arr =String(Math.abs(n)).split("").map(element=>+element);
+    arr.reverse();
+    let result = +arr.join("");
+    return result;
 }
